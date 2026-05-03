a23edaa297ecdc3d914aed9403f6d93afa05b033
jefferson <jeffleyd@gmail.com>
2026-05-03 14:21:53 +0200

    feat: enable allowedHosts option in Vite configuration

--- vite.wasm.config.js
+++ vite.wasm.config.js
@@ -33,6 +33,7 @@ export default defineConfig({
         port: 1420,
         strictPort: true,
         host: '0.0.0.0',
+        allowedHosts: true,
         fs: {
             allow: [nativebladeBase, projectRoot],
         },
