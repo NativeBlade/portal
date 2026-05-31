169254b3269428e914cf446456126bb8ee1008ea
jefferson <jeffleyd@gmail.com>
2026-05-31 14:16:59 +0200

    chore: update version to 1.3.6 and increment version number to 30 in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,8 +12,8 @@ use NativeBlade\Facades\NativeBladeConfig;
 
 class AppServiceProvider extends ServiceProvider
 {
-    CONST VERSION = '1.3.5';
-    const VERSION_NUMBER = 29;
+    CONST VERSION = '1.3.6';
+    const VERSION_NUMBER = 30;
 
     public function register(): void
     {
