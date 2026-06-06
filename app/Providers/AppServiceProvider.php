6b3d59b51e6fa7e59e44a6b21e5186ffeec8eecf
jefferson <jeffleyd@gmail.com>
2026-06-06 20:53:13 +0200

    chore: update version to 1.4.6 and increment version number to 39 in AppServiceProvider; update nativeblade version to v2.4.12 in composer.lock; update app version in tauri.conf.json

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,8 +12,8 @@ use NativeBlade\Facades\NativeBladeConfig;
 
 class AppServiceProvider extends ServiceProvider
 {
-    CONST VERSION = '1.4.5';
-    const VERSION_NUMBER = 38;
+    CONST VERSION = '1.4.6';
+    const VERSION_NUMBER = 39;
 
     public function register(): void
     {
