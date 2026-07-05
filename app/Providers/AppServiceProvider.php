cf04ae6e312c6eee8db1e2f2f26de351cb6db1a7
Jefferson T.S <jeffleyd@gmail.com>
2026-07-06 00:08:59 +0200

    chore: increment version to 1.8.1 and version number to 1008001 in AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,8 @@ use NativeBlade\Facades\NativeBladeConfig;
 
 class AppServiceProvider extends ServiceProvider
 {
-    CONST VERSION = '1.8.0';
-    const VERSION_NUMBER = 1008000;
+    CONST VERSION = '1.8.1';
+    const VERSION_NUMBER = 1008001;
 
     public function register(): void
     {
