9c35d9d226f26267df60121b6d938d4987eb271c
Jefferson T.S <jeffleyd@gmail.com>
2026-09-16 21:01:55 +0200

    chore: increment version to 2.9.1 and version number to 2000901 in AppServiceProvider.php; update nativeblade package version to v37.6.3 in composer.lock; add tauri-plugin-nativeblade-system dependency in Cargo.lock; update version in tauri.conf.json to 2.9.0

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,8 @@ use NativeBlade\Facades\NativeBladeConfig;
 
 class AppServiceProvider extends ServiceProvider
 {
-    CONST VERSION = '2.9.0';
-    const VERSION_NUMBER = 2000900;
+    CONST VERSION = '2.9.1';
+    const VERSION_NUMBER = 2000901;
 
     public function register(): void
     {
